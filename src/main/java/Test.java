bb562ea64573db0faf8d7b2cd3b9d533153f684b
mihirchhaya <mihir.chhaya@gmail.com>
2023-07-18 12:19:10 -0400

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,1 +1,12 @@
+import org.mybatis.cdi.Transactional;
 
+public class Test {
+    public static void main(){
+    
+    }
+    
+   @Transactional
+   public void saveTest(){
+       System.out.println ("Save information");
+   }
+}
